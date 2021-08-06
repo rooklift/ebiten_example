d6c0f40a057d5b3d571486404b709f238e0e95b9
rooklift <16438795+rooklift@users.noreply.github.com>
2021-08-06 17:47:31 +0100

    initial

---
+++ foo.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"image/png"
+	"os"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	w = 600
+	h = 400
+)
+
+var powerup *ebiten.Image
+
+func init() {
+
+	f, err := os.Open("powerup.png")
+	if err != nil {
+		panic("Couldn't read infile")
+	}
+	defer f.Close()
+
+	img, err := png.Decode(f)
+	if err != nil {
+		panic("Couldn't parse infile")
+	}
+
+	powerup = ebiten.NewImageFromImage(img)
+}
+
+type Game struct{
+	image *ebiten.Image
+
+	inited bool
+	width int
+	height int
+	px int
+	py int
+	speedx int
+	speedy int
+}
+
+func (g *Game) DrawSprite(x int, y int, img *ebiten.Image) {
+
+	e_width, e_height := img.Size()
+
+	opts := new(ebiten.DrawImageOptions)
+	opts.GeoM.Translate(float64(x) - (float64(e_width) / 2), float64(y) - (float64(e_height) / 2.0))
+
+	g.image.DrawImage(img, opts)
+}
+
+func (g *Game) Update() error {
+
+	if (!g.inited) {
+
+		g.width = w
+		g.height = h
+		g.image = ebiten.NewImage(g.width, g.height)
+
+		g.speedx = 2
+		g.speedy = 1
+
+		g.inited = true
+	}
+
+	if (g.px < 0) { g.speedx = 2 }
+	if (g.px >= g.width) { g.speedx = -2 }
+	if (g.py < 0) { g.speedy = 1 }
+	if (g.py >= g.height) { g.speedy = -1 }
+
+	g.px += g.speedx
+	g.py += g.speedy
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+
+	g.image.Clear()
+	g.DrawSprite(g.px, g.py, powerup)
+
+	screen.DrawImage(g.image, nil)
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f -- FPS: %0.2f", ebiten.CurrentTPS(), ebiten.CurrentFPS()))
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return w, h
+}
+
+func main() {
+
+	g := new(Game)
+
+	ebiten.SetWindowSize(w * 2, h * 2)
+	ebiten.SetWindowTitle("Foo")
+	if err := ebiten.RunGame(g); err != nil {
+		panic(err)
+	}
+}
